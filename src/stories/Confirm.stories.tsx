19caf92145c5e1eac1f340108ec3a2f2246b69e8
Marcio Antonio dos Santos <marcioasan@gmail.com>
2021-10-11 20:21:49 -0300

    4.18. Desafio - Componente Confirm

---
+++ src/stories/Confirm.stories.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Story, Meta } from '@storybook/react';
+
+import Confirm, { ConfirmProps } from '../components/Confirm/Confirm';
+
+export default {
+  title: 'Example/Confirm',
+  component: Confirm,
+  // argTypes: {
+  //   backgroundColor: { control: 'color' },
+  // },
+} as Meta;
+
+const Template: Story<ConfirmProps> = (args) => <Confirm {...args} />;
+
+export const Default = Template.bind({})
+Default.args = {
+  title: 'Deseja realmente sair?'
+}
