44d094ee5972fafa9e7483e6118134f8bb33e0da
Marcio Antonio dos Santos <marcioasan@gmail.com>
2021-10-13 21:50:11 -0300

    Aulas 4.21 e 4.22

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,7 +1,11 @@
+import { mdiOpenInNew } from "@mdi/js"
+import Icon from "@mdi/react"
 import { useMemo } from "react"
 import { Column, useTable } from 'react-table'
+import * as T from './Table.styles'
 
 type Data = {
+  preview: React.ReactNode
   col1: string
   col2: string
   actions: string
@@ -14,17 +18,30 @@ export default function Table() {
       {
         col1: 'Hello',
         col2: 'World',
-        actions: 'ações'
+        actions: 'ações',
+        preview: <Icon
+          size="14px"
+          color="#09F"
+          path={ mdiOpenInNew }
+        />
       },
       {
         col1: 'react-table',
         col2: 'rocks',
-        actions: 'ações'
+        actions: 'ações',
+        preview: <Icon
+          size="14px" 
+          path={ mdiOpenInNew }
+        />
       },
       {
         col1: 'whatever',
         col2: 'you want',
-        actions: 'ações'
+        actions: 'ações',
+        preview: <Icon
+          size="14px" 
+          path={ mdiOpenInNew }
+        />
       },
     ],
     []
@@ -33,12 +50,20 @@ export default function Table() {
   const columns = useMemo<Column<Data>[]>(
     () => [
       {
+        Header: '',
+        accessor: 'preview', // accessor is the "key" in the data
+      },
+      {
         Header: 'Column 1',
-        accessor: 'col1', // accessor is the "key" in the data
+        accessor: 'col1',
+        width: 320,
+        Cell: (row) => <div style={{ textAlign: 'right' }}>{ row.value }</div>
+        
       },
       {
         Header: 'Column 2',
         accessor: 'col2',
+        Cell: (row) => <div style={{ textAlign: 'center' }}>{ row.value }</div>
       },
       {
         Header: 'Ações',
@@ -57,40 +82,40 @@ export default function Table() {
     } = useTable<Data>({ data, columns })
 
     return(
-      <table {...getTableProps}>
-        <thead>
+      <T.Wrapper cellPadding={0} cellSpacing={0} {...getTableProps}>
+        <T.Heading>
           {
             headerGroups.map(headerGroup => (
-              <tr {...headerGroup.getHeaderGroupProps()}>
+              <T.HeadingRow {...headerGroup.getHeaderGroupProps()}>
                 {
                   headerGroup.headers.map(column => (
-                    <th {...column.getHeaderProps()}>
+                    <T.HeadingCell {...column.getHeaderProps()}>
                       {column.render('Header')}
-                    </th>
+                    </T.HeadingCell>
                   ))
                 }
-              </tr>
+              </T.HeadingRow>
             ))
           }
-        </thead>
-        <tbody {...getTableBodyProps()}>
+        </T.Heading>
+        <T.Body {...getTableBodyProps()}>
           {
             rows.map(row => {
               prepareRow(row)
-              return <tr {...row.getRowProps()}>
+              return <T.BodyRow {...row.getRowProps()}>
                 {
                   row.cells.map(cell => {
-                    return <td {...cell.getCellProps()}>
+                    return <T.BodyCell {...cell.getCellProps()}>
                       { cell.render('Cell') }                      
-                    </td>
+                    </T.BodyCell>
                   })
                 }
 
-              </tr>
+              </T.BodyRow>
             })
           }
-        </tbody>
-      </table>
+        </T.Body>
+      </T.Wrapper>
     )
 
 }
