2b62339ff90142da430f7be1311f7e15734d2449
Marcio Antonio dos Santos <marcioasan@gmail.com>
2021-12-07 20:39:53 -0300

    4.47. Desafio profile

---
+++ src/stories/Profile.stories.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Story, Meta } from '@storybook/react';
+
+import Profile, { ProfileProps } from '../components/Profile/Profile';
+
+export default {
+  title: 'Example/Profile',
+  component: Profile,
+} as Meta;
+
+const Template: Story<ProfileProps> = (args) => <Profile {...args} />;
+
+export const Default = Template.bind({})
+Default.args = {
+  name: 'Márcio Antonio dos Santos',
+  description: 'Programador Java'
+}
