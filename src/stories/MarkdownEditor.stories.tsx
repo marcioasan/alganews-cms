268fdf8b98487359770e543ac20e7950e4e55682
Marcio Antonio dos Santos <marcioasan@gmail.com>
2022-01-09 13:15:13 -0300

    4.54. Editor de texto em Markdown

---
+++ src/stories/MarkdownEditor.stories.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Story, Meta } from '@storybook/react';
+
+import MarkdownEditor, { MarkdownEditorProps } from '../components/MarkdownEditor/MarkdownEditor';
+
+export default {
+  title: 'Example/MarkdownEditor',
+  component: MarkdownEditor,
+} as Meta;
+
+const Template: Story<MarkdownEditorProps> = (args) => <MarkdownEditor {...args} />;
+
+export const Default = Template.bind({})
+Default.args = {}
