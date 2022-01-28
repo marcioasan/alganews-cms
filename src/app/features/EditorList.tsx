c0780b78a39e088c5194579cbfe3bfd9bfe83118
Marcio Antonio dos Santos <marcioasan@gmail.com>
2022-01-28 13:13:15 -0300

    6.19. Estilizando um componente de uma biblioteca externa

--- src/app/features/EditorList.tsx
+++ src/app/features/EditorList.tsx
@@ -3,16 +3,16 @@ import Profile from "../components/Profile";
 
 export default function EditorsList() {
   return <EditorsListWrapper>
-    <Profile name="Márcio Antonio dos Santos" description="Dev há mais de 10 anos" />
-    <Profile name="Tospericajerja" description="Dev há 3 anos" />
-    <Profile name="Jacinto Leite Aquino Rego" description="Dev há mais de 30 anos" />
-    <Profile name="Krahenbul Losk Vron" description="Dev há 5 anos" />
-    <Profile name="Javiolandia da Silva" description="Dev há 8 anos" />
+    <Profile editorId={1} name="Márcio Antonio dos Santos" description="Dev há mais de 10 anos" />
+    <Profile editorId={2}  name="Tospericajerja" description="Dev há 3 anos" />
+    <Profile editorId={3}  name="Jacinto Leite Aquino Rego" description="Dev há mais de 30 anos" />
+    <Profile editorId={4}  name="Krahenbul Losk Vron" description="Dev há 5 anos" />
+    <Profile editorId={5}  name="Javiolandia da Silva" description="Dev há 8 anos" />
   </EditorsListWrapper>
 }
 
 const EditorsListWrapper = styled.div`
-  display: grid;
+  display: grid; 
   grid-template-columns: repeat(2, 1fr);
   gap: 24px;
 `
