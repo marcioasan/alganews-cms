0602eb2035a456888fab3c4941496fb4a3a7639c
Marcio Antonio dos Santos <marcioasan@gmail.com>
2022-02-08 21:57:12 -0300

    8.15. Enviando dados (método POST) com axios

--- src/sdk/services/Post.service.ts
+++ src/sdk/services/Post.service.ts
@@ -7,14 +7,21 @@ class PostService extends Service {
     return this.Http
       .get<Post.Paginated>('/posts')
       .then(this.getData)
+      //.then(res => res.data) o método then recebe como parâmetro a resposta do get "res" e devolve os dados da resposta "res.data"
+      //Para não ter sempre que escrever "res => res.data", esse retorno foi colocado em um método "getData" no Service.ts.
   }
 
   static getExistingPost(id: number) {    
     return this.Http
       .get<Post.Paginated>(`/posts/${id}`)
       .then(this.getData) 
-      //.then(res => res.data) o método then recebe como parâmetro a resposta do get "res" e devolve os dados da resposta "res.data"
-      //Para não ter sempre que escrever "res => res.data", esse retorno foi colocado em um método "getData" no Service.ts.
+  }
+
+  //8.15. Enviando dados (método POST) com axios
+  static insertNewPost(post: Post.Input) {
+    return this.Http
+      .post<Post.Datailed>('/post', post)
+      .then(this.getData)
   }
 }
 
