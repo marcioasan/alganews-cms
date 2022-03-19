45393aad044edd5fecd55e416fb2f465d75d2d37
Marcio Antonio dos Santos <marcioasan@gmail.com>
2022-03-19 09:18:26 -0300

    8.48. Abrindo links em uma nova aba

--- src/app/components/MarkdownEditor/MarkdownEditor.tsx
+++ src/app/components/MarkdownEditor/MarkdownEditor.tsx
@@ -6,6 +6,24 @@ MdEditor.unuse(Plugins.FontUnderline)
 
 const parser = new MarkdownIt()
 
+//8.48. Abrindo links em uma nova aba - INÍCIO
+const defaultRender = parser.renderer.rules.link_open ||
+  function(tokens: any, idx: any, options: any, env: any, self: any) {
+    return self.renderToken(tokens, idx, options);
+  };
+
+parser.renderer.rules.link_open = function (tokens, idx, options, env, self) {
+  var aIndex = tokens[idx].attrIndex('target');
+  if (aIndex < 0) {
+    tokens[idx].attrPush(['target', '_blank']);
+  } else {
+    //@ts-ignore
+    tokens[idx].attrs[aIndex][1] = '_blank';
+  }
+  return defaultRender(tokens, idx, options, env, self);
+};
+//8.48. Abrindo links em uma nova aba - FIM
+
 export interface MarkdownEditorProps {
   onChange?: (text: string) => any
   value?: string;
