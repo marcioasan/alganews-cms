836aab4f9341fa52d3c9c36f773d143588308df4
Marcio Antonio dos Santos <marcioasan@gmail.com>
2022-03-18 21:56:21 -0300

    8.46. Desafio - Layout do Post

--- src/app/features/PostPreview.tsx
+++ src/app/features/PostPreview.tsx
@@ -1,6 +1,7 @@
 import withBoundary from "../../core/hoc/withBoundary";
 import styled from 'styled-components'
 import MarkdownEditor from '../components/MarkdownEditor/MarkdownEditor';
+import Button from "../components/Button/Button";
 
 //8.44. Desafio - Criar modal de Preview de Post
 interface PostPreviewProps {
@@ -8,22 +9,66 @@ interface PostPreviewProps {
 }
 
 function PostPreview (props: PostPreviewProps) {
-  return <Wrapper>
-    features/PostPreview
-    <MarkdownEditor
-      readOnly
-      value={'ola mundo\n- esta é\n- uma lista'} 
-    />
-  </Wrapper>
+
+  //8.46. Desafio - Layout do Post
+  return <PostPreviewWrapper>
+      <PostPreviewHeading>
+        <PostPreviewTitle>
+          {'Como fiquei rico aprendendo React'}
+        </PostPreviewTitle>
+        <PostPreviewActions>
+          <Button
+            variant={'danger'}
+            label={'Publicar'}
+          />
+          <Button
+            variant={'primary'}
+            label={'Editar'}
+          />          
+        </PostPreviewActions>
+      </PostPreviewHeading>
+      <PostPreviewImage
+        src={'https://images.unsplash.com/photo-1499343628900-545067aef5a3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80'}
+      />
+      <PostPreviewContent>
+        <MarkdownEditor
+          readOnly
+          value={'ola mundo\n- esta é\n- uma lista'} 
+        />
+      </PostPreviewContent>
+  </PostPreviewWrapper>
 }
 
-  const Wrapper = styled.div`
-    display: flex;
-    flex-direction: column;
-    gap: 24px;
-    width: 655px;
-    background-color: #F3F8FA;
-    border: 1px solid #ccc;
-    padding: 24px;
-  `
+const PostPreviewWrapper = styled.div`
+  background-color: #f3f8fa;
+  padding: 24px;
+  border: 1px solid #ccc;
+  width: 655px;
+  display: flex;
+  gap: 24px;
+  flex-direction: column;
+  max-height: 70vh;
+  overflow-y: auto;
+  box-shadow: 0 6px 6px rgba(0,0,0,.05);
+`
+
+const PostPreviewHeading = styled.div`
+  display: flex;
+  justify-content: space-between;
+`
+const PostPreviewTitle = styled.h2`
+  font-size: 18px;
+  font-weight: 600;
+`
+const PostPreviewActions = styled.div`
+  display: flex;
+  gap: 8px;
+`
+const PostPreviewImage = styled.img`
+  height: 240px;
+  width: 100%;
+  object-fit: cover;
+`
+const PostPreviewContent = styled.div`
+`
 export default withBoundary(PostPreview)
